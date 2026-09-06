bf205cbefad83798aa39673195d49ce17198e757
Md Ashiqul Islam <mdashiqislam2022@gmail.com>
2026-09-06 22:33:52 +0600

    Update translation for cash counting to cash analysis

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -6,7 +6,7 @@ export const t = {
     dashboard: 'Dashboard',
         loanAccounts: 'Loan Account Details',
     chequeCardRegistry: 'Cheque & Debit Cards',
-    denominationSegregation: 'Cash Counting',
+        denominationSegregation: 'Cash Analysis',
     userDirectory: 'AFO Directory',
     afoManagement: 'AFO Directory',
     afoTransfer: 'AFO Transfer',
