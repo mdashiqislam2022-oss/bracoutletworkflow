635cbd26261e52456c1b6e3668c57f45645f05d5
Md Ashiqul Islam <mdashiqislam2022@gmail.com>
2026-09-07 21:41:37 +0600

    Simplify condition for setting motherAmounts

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -435,7 +435,7 @@ export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children 
           if (cloudData.auditLogs) setAuditLogs(cloudData.auditLogs);
           if (cloudData.governanceSettings) setGovernanceSettings(cloudData.governanceSettings);
           if (cloudData.segregationRecords) setSegregationRecords(cloudData.segregationRecords);
-                   if (cloudData.motherAmounts && cloudData.motherAmounts.length) setMotherAmounts(cloudData.motherAmounts);
+                  if (cloudData.motherAmounts) setMotherAmounts(cloudData.motherAmounts);
           if (cloudData.outletTransfers && cloudData.outletTransfers.length) setOutletTransfers(cloudData.outletTransfers);
           if (cloudData.outletVaults && cloudData.outletVaults.length) setOutletVaults(cloudData.outletVaults);
                     if (cloudData.cashTransfers && cloudData.cashTransfers.length) setCashTransfers(cloudData.cashTransfers);
