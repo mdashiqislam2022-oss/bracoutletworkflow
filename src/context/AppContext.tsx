79de6b1fd05aa2b8e4c9956d5e41ac6e5a1cb3fc
Md Ashiqul Islam <mdashiqislam2022@gmail.com>
2026-09-07 21:42:40 +0600

    Refactor condition for setting motherAmounts

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -508,7 +508,7 @@ export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children 
             if (cloudData.loanRecords) setLoanRecords(cloudData.loanRecords);
             if (cloudData.customerAccounts) setCustomerAccounts(cloudData.customerAccounts);
             if (cloudData.segregationRecords) setSegregationRecords(cloudData.segregationRecords);
-                        if (cloudData.motherAmounts && cloudData.motherAmounts.length) setMotherAmounts(cloudData.motherAmounts);
+             if (cloudData.motherAmounts) setMotherAmounts(cloudData.motherAmounts);
             if (cloudData.outletTransfers && cloudData.outletTransfers.length) setOutletTransfers(cloudData.outletTransfers);
             if (cloudData.outletVaults && cloudData.outletVaults.length) setOutletVaults(cloudData.outletVaults);
                       if (cloudData.cashTransfers && cloudData.cashTransfers.length) setCashTransfers(cloudData.cashTransfers);
