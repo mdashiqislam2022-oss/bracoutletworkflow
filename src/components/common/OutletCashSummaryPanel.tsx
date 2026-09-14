ffd971b0638f07e93bcd451381024784bdb009bd
Md Ashiqul Islam <mdashiqislam2022@gmail.com>
2026-09-14 21:42:20 +0600

    Add supportingRecords to OutletCashSummaryPanel

--- src/components/common/OutletCashSummaryPanel.tsx
+++ src/components/common/OutletCashSummaryPanel.tsx
@@ -24,7 +24,7 @@ interface OutletCashSummaryPanelProps {
 }
 
 export const OutletCashSummaryPanel: React.FC<OutletCashSummaryPanelProps> = ({ outletId, isDark }) => {
-  const { segregationRecords, motherAmounts, outletTransfers, cashTransfers } = useApp();
+ const { segregationRecords, motherAmounts, outletTransfers, cashTransfers, supportingRecords } = useApp();
 
   const cardBg = isDark ? 'bg-[#1A2333] border-slate-800' : 'bg-white border-slate-200';
 
