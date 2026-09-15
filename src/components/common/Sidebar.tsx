6a84f2be856250f2f582a5fdfaf9bebcddacb05a
Md Ashiqul Islam <mdashiqislam2022@gmail.com>
2026-09-15 06:09:18 +0600

    Add 'Supporting' item to Sidebar component

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -60,7 +60,8 @@ export const Sidebar: React.FC = () => {
         { id: 'cheque_cards', icon: CreditCard, label: langText.chequeCardRegistry },
         { id: 'loan_accounts', icon: Landmark, label: langText.loanAccounts },
                                 { id: 'denomination_segregation', icon: Calculator, label: 'Cash Counting' },
-                { id: 'cash_transfer', icon: ArrowRightLeft, label: 'Transfer & Analysis' },
+                      { id: 'cash_transfer', icon: ArrowRightLeft, label: 'Transfer & Analysis' },
+        { id: 'supporting', icon: HandCoins, label: 'Supporting' },
         { id: 'note_rule', icon: StickyNote, label: 'Note & Rule' }
       ]
     },
